1bbbee1b98dff513c563f9211297875aae638ae3
lzy1029321711 <1029321711@qq.com>
2017-11-19 22:57:46 +0800

    emmmmmmmmm

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -0,0 +1,33 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan.dao;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by 天地科技 on 2017/11/15.
+ */
+
+public class AntiVirusDao {
+    private static String dbname;
+    private static Context context;
+    public AntiVirusDao(Context context){
+        this.context=context;
+        dbname="/data/data/"+context.getPackageName()+"/files/antivirus.db";
+    }
+    public String checkVirus(String md5){
+        String desc=null;
+        SQLiteDatabase db=SQLiteDatabase.openDatabase(
+                dbname,null,
+        SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor=db.rawQuery("select desc from datable where md5=?",
+                new String[] {md5});
+        if (cursor.moveToNext()){
+            desc = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return desc;
+    }
+
+}
